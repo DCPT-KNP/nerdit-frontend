c499ce1a6b3bec4ecb33a44227249b2f2e231ac5
centraldogma99 <central.dogma99@gmail.com>
2022-09-12 23:10:09 +0900

    fix: GraphPart 수정

--- components/Graph/GraphPart.tsx
+++ components/Graph/GraphPart.tsx
@@ -1,3 +1,4 @@
+import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import React from 'react'
 
@@ -5,6 +6,7 @@ import { Skill } from '../../constants/skills'
 import palette from '../../foundation/palette'
 import Typo from '../../foundation/typography'
 import pxToRem from '../../utils/pxToRem'
+import Flex from '../Flex'
 import { Colors } from './constants'
 
 const CONTAINER_WIDTH = pxToRem(126)
@@ -18,29 +20,35 @@ interface Props {
 
 const GraphPart = ({ variant, skill }: Props) => {
   return (
-    <Container variant={variant} skill={skill}>
-      <Text type="title1" color={palette.Base.White}>
+    <Container
+      variant={variant}
+      skill={skill}
+      flexDirection="column"
+      justifyContent={variant === 'big' ? 'flex-end' : 'center'}
+    >
+      <Text type="header1" color={palette.Base.White} variant={variant}>
         {skill}
       </Text>
     </Container>
   )
 }
 
-const Container = styled.div<Props>`
+const Container = styled(Flex)<Props>`
   width: ${CONTAINER_WIDTH}rem;
   height: ${({ variant }) =>
     variant === 'big'
       ? `${CONTAINER_HEIGHT_LARGE}rem`
       : `${CONTAINER_HEIGHT_SMALL}rem`};
   background-color: ${({ skill }) => Colors[skill]};
-  display: flex;
-  justify-content: flex-end;
-  flex-direction: column;
 `
 
-const Text = styled(Typo)`
+const Text = styled(Typo)<Pick<Props, 'variant'>>`
   color: ${palette.Base.White};
-  margin-bottom: pxToRem(24) rem;
+  ${({ variant }) =>
+    variant === 'big' &&
+    css`
+      margin-bottom: ${pxToRem(24)}rem;
+    `}
   width: 100%;
   text-align: center;
 `
